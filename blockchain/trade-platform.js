5f49f19b9237e9cf23f6d48fd57593b408d2607c
Mihnea Duican <mduican@adobe.com>
2023-01-18 23:42:30 +0200

    some commit

--- blockchain/trade-platform.js
+++ blockchain/trade-platform.js
@@ -3,7 +3,7 @@ import contract_abi from "../blockchain/abi/EvChargingMarket.json"
 const abi = contract_abi.abi;
 
 const tradingContract = web3 => {
-    return new web3.eth.Contract(abi, "0xc00fE9a0DBE760332a96Cc4CFa62FD6523E42E0e")
+    return new web3.eth.Contract(abi, "0x9d81A7edbF98e3A30EfCc8a6f68c916af5Fd0F3e")
 }
 
 export default tradingContract
